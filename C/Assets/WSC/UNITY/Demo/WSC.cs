c273b0a099706d4ed0b96b88ddfa40f5af7336eb
icoder <icodes.studio@gmail.com>
2023-09-21 11:49:46 +0900

    unit-test

--- C/Assets/WSC/UNITY/Demo/WSC.cs
+++ C/Assets/WSC/UNITY/Demo/WSC.cs
@@ -27,54 +27,53 @@ namespace WSC.UNITY.DEMO
                 }
             });
 
-            //Network.i.Initialize();
             Network.i.Initialize(new global::WSC.UNITY.WebProtocolFactory());
         }
 
         private void Start()
         {
             new RequestWSCLogin().Query();
-            //new RequestWSCLogin().Query<AnswerWSCLogin>((answer) =>
-            //{
-            //    Log.Debug($"[TEST#1] timestamp: {answer.timestamp}, token: {answer.token}");
-            //});
+            new RequestWSCLogin().Query<AnswerWSCLogin>((answer) =>
+            {
+                Log.Debug($"[TEST#1] timestamp: {answer.timestamp}, token: {answer.token}");
+            });
 
-            //new RequestWSCTime().Query<AnswerWSCTime>().Done += ((answer) =>
-            //{
-            //    if (answer is AnswerWSCTime time)
-            //        Log.Debug($"[TEST#2] timestamp: {time.timestamp}");
-            //});
+            new RequestWSCTime().Query<AnswerWSCTime>().Done += ((answer) =>
+            {
+                if (answer is AnswerWSCTime time)
+                    Log.Debug($"[TEST#2] timestamp: {time.timestamp}");
+            });
 
-            //new RequestWSCHello() { message = "Hello" }.Query<AnswerWSCHello>((answer) =>
-            // {
-            //     Log.Debug($"[TEST#3] message: {answer.message}");
-            // });
+            new RequestWSCHello() { message = "Hello" }.Query<AnswerWSCHello>((answer) =>
+            {
+                Log.Debug($"[TEST#3] message: {answer.message}");
+            });
 
-            //new RequestWSCLogin().Query<Answer>((answer) =>
-            //{
-            //    if (answer.error == 0)
-            //    {
-            //        new RequestWSCTime().Query<Answer>((answer) =>
-            //        {
-            //            if (answer.error == 0)
-            //            {
-            //                new RequestWSCHello() { message = "Hello" }.Query<Answer>((answer) =>
-            //                 {
-            //                     Log.Debug($"[TEST#4] error: {answer.error}");
-            //                 });
-            //            }
-            //        });
-            //    }
-            //});
+            new RequestWSCLogin().Query<Answer>((answer) =>
+            {
+                if (answer.error == 0)
+                {
+                    new RequestWSCTime().Query<Answer>((answer) =>
+                    {
+                        if (answer.error == 0)
+                        {
+                            new RequestWSCHello() { message = "Hello" }.Query<Answer>((answer) =>
+                             {
+                                 Log.Debug($"[TEST#4] error: {answer.error}");
+                             });
+                        }
+                    });
+                }
+            });
 
-            //new NetworkTask()
-            //    .Bind(new RequestWSCLogin().Query<AnswerWSCLogin>())
-            //    .Bind(new RequestWSCTime().Query<AnswerWSCTime>())
-            //    .Bind(new RequestWSCHello() { message = "Hello" }.Query<AnswerWSCHello>())
-            //    .Done += exception =>
-            //    {
-            //        Log.Debug($"TEST#5 error message: {exception?.ToString()}");
-            //    };
+            new NetworkTask()
+                .Bind(new RequestWSCLogin().Query<AnswerWSCLogin>())
+                .Bind(new RequestWSCTime().Query<AnswerWSCTime>())
+                .Bind(new RequestWSCHello() { message = "Hello" }.Query<AnswerWSCHello>())
+                .Done += exception =>
+                {
+                    Log.Debug($"TEST#5 error message: {exception?.ToString()}");
+                };
         }
     }
 }
